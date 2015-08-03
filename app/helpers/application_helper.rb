d16412104dea7def5aa88d9b3f505e48b9f257b1
AlexanderPower <aleksandr.power88@gmail.com>
2015-08-03 12:56:42 +0500

    Added main user behavior integration test.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def icon_for_favorite(model)
     star_class= model.marked_as?(:favorite, by: current_user) ? 'fa-star' : 'fa-star-o'
-    content_tag :i,'', class: "fa fa-2x #{star_class}", data: {id: model.id, klass: model.class.to_s}
+    content_tag :i,'', class: "fa fa-2x #{star_class} #{model.class.to_s.downcase}-#{model.id}", data: {id: model.id, klass: model.class.to_s}
   end
 end
---
+++ spec/features/main_behavior_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe 'main user behavior' do
+  it 'sign_up with valid email and password' do
+    user= build :user
+    sign_up_with user.email, user.password
+    expect(page).to have_content('My favorites')
+  end
+  context 'user actions' do
+    let!(:company1) { create :company_with_people }
+    let!(:company2) { create :company_with_people }
+    let(:person1) { company1.people[0] }
+    let(:person2) { company1.people[1] }
+    before(:each) { sign_in }
+
+    context 'vist pages' do
+      it 'root path' do
+        visit root_path
+        expect(page).to have_content company1.name
+        expect(page).to have_content company2.name
+      end
+      it 'company1 path' do
+        visit company_path company1
+        company1.people.each do |person|
+          expect(page).to have_content person.name
+        end
+      end
+    end
+
+    it 'user added company and people to favorite', js: true do
+      visit root_path
+      # click by empty star
+      # find("[data-id='#{company1.id}']").click
+      find(".company-#{company1.id}").click
+      wait_for_ajax
+      expect(page).to have_css '.fa-star'
+
+      visit company_path company1
+      find(".person-#{person1.id}").click
+      find(".person-#{person2.id}").click
+      wait_for_ajax
+
+      visit favorites_path
+      expect(page).to have_content company1.name
+      expect(page).to have_content person1.name
+      expect(page).to have_content person2.name
+    end
+  end
+end
